06307888c0fbba058f607ee7ca5fbe5f95ca7f37
Samuele Fiorini <samuele.fiorini@gmail.com>
2017-01-15 14:30:23 +0100

    restyle of the plotting utility and new functions

--- cgmtools/plotting.py
+++ cgmtools/plotting.py
@@ -12,32 +12,37 @@ import statsmodels.api as sm
 __all__ = ['cgm', 'autocorrelation']
 
 
-def cgm(time, gluco, hypo=70, hyper=126,
-        title="Patiend ID glucose level"):
+def cgm(df, gluco_fit=None, hypo=70, hyper=126,
+        title="Patiend ID CGM", savefig=False):
     """Plot the CGM signal on an input time span.
 
     Parameters
     -------------------
-    time : array of datetimes, the horizontal axis
-    gluco : array of float, the correspondent glucose
-            level on the vertical axis
+    df : DataFrame, the output returned by gluco_extract(..., return_df=True)
+    gluco_fit : array of float, the results of a fitted model (optional)
     hypo : number, hypoglicaemia threshold in
            mg/dl (default is 70, i.e. 3.9 mmol/l)
     hyper : number, hyperglicaemia threshold
             in mg/dl (default is 126, i.e. 7 mmol/l)
     title : string, the title of the plot (optional)
+    savefig : bool, if True save title.png
     """
-    plt.figure(figsize=(10, 6))
-    plt.hlines(hypo, time[0], time[-1], linestyles='dashed',
+    plt.figure(figsize=(10, 6), dpi=300)
+    plt.hlines(hypo, df.index[0], df.index[-1], linestyles='dashed',
                label='hypoglicaemia')
-    plt.hlines(hyper, time[0], time[-1], linestyles='dotted',
+    plt.hlines(hyper, df.index[0], df.index[-1], linestyles='dotted',
                label='hyperglicaemia')
     plt.ylim([10, 410])
-    plt.plot_date(time, gluco, '-', label='glucose level')
+    plt.plot_date(df.index, df.as_matrix(),
+                  linestyle='solid', label='real CGM')
+    if gluco_fit is not None:
+        plt.plot(df.index, gluco_fit,
+                 linestyle='dashed', label='predicted CGM')
     plt.title(title)
     plt.ylabel('mg/dL')
     plt.xticks(rotation='vertical')
-    plt.legend()
+    plt.legend(bbox_to_anchor=(1.2, 1.0))
+    if savefig: plt.savefig(title+'_fit.png')
 
 
 def autocorrelation(ts, lags=None):
@@ -48,12 +53,35 @@ def autocorrelation(ts, lags=None):
     ts : array of float, time-series values
     lags : array of int, lag values on horizontal axis.
     """
-    fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 8), dpi=300)
     ax1 = fig.add_subplot(211)
     fig = sm.graphics.tsa.plot_acf(ts, lags=lags, ax=ax1)
     ax2 = fig.add_subplot(212)
     fig = sm.graphics.tsa.plot_pacf(ts, lags=lags, ax=ax2)
     plt.suptitle('Lags: {}'.format(lags))
+
+
+def residuals(df, forecast, skip_first=0, skip_last=0,
+              title="Patiend ID CGM", savefig=False):
+    """Plot the input residuals.
+
+    Parameters
+    -------------------
+    df : DataFrame, the output returned by gluco_extract(..., return_df=True)
+    forecast : array of float, the prediction for the given time-series
+    skip_first : number, the number of initial samples to exclude
+    skip_last : number, the number of final samples to exclude
+    title : string, the title of the plot (optional)
+    savefig : bool, if True save title.png
+    """
+    # Evaluate the residuals (exclude learning and open loop ph samples)
+    residuals = df.as_matrix()[skip_first:-skip_last].ravel() - forecast['ts'][skip_first:-skip_last]
+
+    plt.figure(figsize=(12, 4), dpi=300)
+    plt.plot(df.index[skip_first:-skip_last], residuals)
+    DW = sm.stats.durbin_watson(residuals)
+    plt.title('Durbin-Watson: {:.3f}'.format(DW))
+    if savefig: plt.savefig(title+'_residuals.png')
 
 ###############################################################################
 
